c03f76a1e0566150cd96fc12e237dc94560b7b90
dnorhoj <daniel.norhoj@gmail.com>
2021-09-08 18:44:05 +0200

    Modernized, BS5, better js. Also, h.

---
+++ assets/h.js
@@ -0,0 +1,42 @@
+DarkReader.setFetchMethod(window.fetch);
+DarkReader.auto();
+
+document.onload = () => {
+	let h = document.getElementById('h');
+	let hh = document.getElementById('hh');
+
+	document.getElementById("hhh").addEventListener('click', () => {
+		hh.value = HH(stringToBinary(h.value));
+	});
+	
+	document.getElementById("hhhh").addEventListener('click', () => {
+		if(h.value.match(/[^Hh\n ]/g) || h.value.length % 8 != 0) {
+			new bootstrap.Toast(document.querySelector("#hhhhhhh")).show();
+			hh.value = "";
+			return;
+		}
+		hh.value = binaryToString(H(h.value));
+	});
+
+	document.getElementById("hhhhh").addEventListener('click', () => {
+		navigator.clipboard.writeText(hh.value).then(() => {
+			new bootstrap.Toast(document.querySelector('#hhhhhh')).show();
+		});
+	});
+
+	function H(h) {
+		let hh = h;	
+		hh = hh.replace(new RegExp(" ", "g"), "");
+		hh = hh.replace(new RegExp("h", "g"), "0");
+		hh = hh.replace(new RegExp("H", "g"), "1");
+		return hh;
+	}
+
+	function HH(h) {
+		let hh = h;
+		hh = hh.replace(new RegExp(" ", "g"), "");
+		hh = hh.replace(new RegExp("0", "g"), "h");
+		hh = hh.replace(new RegExp("1", "g"), "H");
+		return hh;
+	}
+};
